96f5e5141cf6c86436947165d1c60af247ca5d9d
MarcLan <112602499+MarcLan@users.noreply.github.com>
2023-09-27 10:38:02 +0000

    Added ECS Count module

---
+++ modules/compute/ecsCount/main.tf
@@ -0,0 +1,22 @@
+######################################################################
+# Create multiple ECSs
+######################################################################
+
+# Get Image ID
+data "huaweicloud_images_image" "myimage" {
+  name = var.image_name
+}
+
+resource "huaweicloud_compute_instance" "this" {
+  count = var.quantity
+
+  name               = format("%s-%s", var.name, count.index)
+  admin_pass         = var.password
+  image_id           = data.huaweicloud_images_image.myimage.id
+  flavor_id          = var.flavor_name
+  security_group_ids = var.security_group
+
+  network {
+    uuid = var.subnet
+  }
+}
--- modules/test/nonprod/ecs.tf
+++ modules/test/nonprod/ecs.tf
@@ -1,24 +1,38 @@
-module "ecs" {
-  source = "../../computing/ecs"
-  ecs = {
+# module "ecs_for_each" {
+#   source = "../../compute/ecsForEach"
+#   ecs = {
 
-    "test1" = {
-      name               = "test-1"
-      password           = "Ly67142325@"
-      flavor_id          = "s7n.small.1"
-      image_name         = "CentOS 8.0 64bit"
-      subnet_id          = module.vpcs.subnet_ids["vpc1.subnet11"]
-      security_group_ids = ["${module.sgs.sg_ids["sg1"]}"]
-    }
+#     "test1" = {
+#       name               = "for_each-1"
+#       password           = "Ly67142325@"
+#       flavor_id     _ids     = "s7n.small.1"
+#       image_name         = "CentOS 8.0 64bit"
+#       subnet_id          = module.vpcs.subnet_ids["vpc1.subnet11"]
+#       security_group_ids = ["${module.sgs.sg_ids["sg1"]}"]
+#     }
 
-    "test2" = {
-      name               = "test-2"
-      password           = "Ly67142325@"
-      flavor_id          = "s7n.small.1"
-      image_name         = "CentOS 8.0 64bit"
-      subnet_id          = module.vpcs.subnet_ids["vpc2.subnet21"]
-      security_group_ids = ["${module.sgs.sg_ids["sg1"]}"]
-    }
+#     "test2" = {
+#       name               = "for_each-2"
+#       password           = "Ly67142325@"
+#       flavor_id          = "s7n.small.1"
+#       image_name         = "CentOS 8.0 64bit"
+#       subnet_id          = module.vpcs.subnet_ids["vpc2.subnet21"]
+#       security_group_ids = ["${module.sgs.sg_ids["sg1"]}"]
+#     }
 
-  }
+#   }
+# }
+
+module "ecs_count" {
+  source = "../../compute/ecsCount"
+
+  enterprise     = module.eps.eps_ids["nonprod"]
+  quantity       = 3
+  name           = "ecs-1"
+  password       = "Ly7142325@"
+  image_name     = "CentOS 8.0 64bit"
+  flavor_name    = "s7n.small.1"
+  subnet         = module.vpcs.subnet_ids["vpc1.subnet11"]
+  security_group = ["${module.sgs.sg_ids["sg1"]}"]
+
 }
