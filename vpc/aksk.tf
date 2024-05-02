9503d89ef2251de170e8c14fee695084876f3770
MarcLan <112602499+MarcLan@users.noreply.github.com>
2024-05-02 10:05:14 +0700

    Delete vpc directory

--- vpc/aksk.tf
+++
@@ -1,5 +0,0 @@
-provider "huaweicloud" {
-  region = "ap-southeast-3"
-  access_key = "4ELEB0YDDNTF7FOU87KW"
-  secret_key = "80vpMFmYZc8YiI8pUTqJgitGYIVMSbpZCgfJxWa1"
-}
--- vpc/sg.tf
+++
@@ -1,9 +0,0 @@
-# module "sgs" {
-#   source = "../modules/network/sg"
-#   sgs = {
-#     "Merchant-Portal-ALB" = {
-#         name = "Merchant-Portal-ALB"
-#         rules = {}
-#     }
-#   }
-# }
